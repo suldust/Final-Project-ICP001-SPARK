d6d8f38f22ec3a1b6bdf52cc498c41439ded1aa7
suldust <minhquangaka@gmail.com>
2024-12-21 13:49:56 +0700

    Snake game.py
    code

--- Snake game.py
+++ Snake game.py
@@ -1,115 +1,193 @@
-import pygame 
-import time
-import random
+import pygame,sys,random
+from pygame.math import Vector2
+class SNAKE:
+	def __init__(self):
+		self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+		self.direction = Vector2(0,0)
+		self.new_block = False
+
+		self.head_up = pygame.image.load('D:/study/KHMT/finalPro/head_up.png').convert_alpha()
+		self.head_down = pygame.image.load('D:/study/KHMT/finalPro/head_down.png').convert_alpha()
+		self.head_right = pygame.image.load('D:/study/KHMT/finalPro/head_right.png').convert_alpha()
+		self.head_left = pygame.image.load('D:/study/KHMT/finalPro/head_left.png').convert_alpha()
+		
+		self.tail_up = pygame.image.load('D:/study/KHMT/finalPro/tail_up.png').convert_alpha()
+		self.tail_down = pygame.image.load('D:/study/KHMT/finalPro/tail_down.png').convert_alpha()
+		self.tail_right = pygame.image.load('D:/study/KHMT/finalPro/tail_right.png').convert_alpha()
+		self.tail_left = pygame.image.load('D:/study/KHMT/finalPro/tail_left.png').convert_alpha()
+
+		self.body_vertical = pygame.image.load('D:/study/KHMT/finalPro/body_vertical.png').convert_alpha()
+		self.body_horizontal = pygame.image.load('D:/study/KHMT/finalPro/body_horizontal.png').convert_alpha()
+
+		self.body_tr = pygame.image.load('D:/study/KHMT/finalPro/body_tr.png').convert_alpha()
+		self.body_tl = pygame.image.load('D:/study/KHMT/finalPro/body_tl.png').convert_alpha()
+		self.body_br = pygame.image.load('D:/study/KHMT/finalPro/body_br.png').convert_alpha()
+		self.body_bl = pygame.image.load('D:/study/KHMT/finalPro/body_bl.png').convert_alpha()
+		self.crunch_sound = pygame.mixer.Sound('D:/study/KHMT/finalPro/crunch.wav')
+	def draw_snake(self):
+		self.update_head_graphics()
+		self.update_tail_graphics()
+
+		for index,block in enumerate(self.body):
+			x_pos = int(block.x * cell_size)
+			y_pos = int(block.y * cell_size)
+			block_rect = pygame.Rect(x_pos,y_pos,cell_size,cell_size)
+
+			if index == 0:
+				screen.blit(self.head,block_rect)
+			elif index == len(self.body) - 1:
+				screen.blit(self.tail,block_rect)
+			else:
+				previous_block = self.body[index + 1] - block
+				next_block = self.body[index - 1] - block
+				if previous_block.x == next_block.x:
+					screen.blit(self.body_vertical,block_rect)
+				elif previous_block.y == next_block.y:
+					screen.blit(self.body_horizontal,block_rect)
+				else:
+					if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+						screen.blit(self.body_tl,block_rect)
+					elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+						screen.blit(self.body_bl,block_rect)
+					elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+						screen.blit(self.body_tr,block_rect)
+					elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+						screen.blit(self.body_br,block_rect)
+
+	def update_head_graphics(self):
+		head_relation = self.body[1] - self.body[0]
+		if head_relation == Vector2(1,0): self.head = self.head_left
+		elif head_relation == Vector2(-1,0): self.head = self.head_right
+		elif head_relation == Vector2(0,1): self.head = self.head_up
+		elif head_relation == Vector2(0,-1): self.head = self.head_down
+
+	def update_tail_graphics(self):
+		tail_relation = self.body[-2] - self.body[-1]
+		if tail_relation == Vector2(1,0): self.tail = self.tail_left
+		elif tail_relation == Vector2(-1,0): self.tail = self.tail_right
+		elif tail_relation == Vector2(0,1): self.tail = self.tail_up
+		elif tail_relation == Vector2(0,-1): self.tail = self.tail_down
+
+	def move_snake(self):
+		if self.new_block == True:
+			body_copy = self.body[:]
+			body_copy.insert(0,body_copy[0] + self.direction)
+			self.body = body_copy[:]
+			self.new_block = False
+		else:
+			body_copy = self.body[:-1]
+			body_copy.insert(0,body_copy[0] + self.direction)
+			self.body = body_copy[:]
+
+	def add_block(self):
+		self.new_block = True
+
+	def play_crunch_sound(self):
+		self.crunch_sound.play()
+
+	def reset(self):
+		self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+		self.direction = Vector2(0,0)
+
+
+class SHARD:
+	def __init__(self):
+		self.randomize()
+
+	def draw_shard(self):
+		shard_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+		screen.blit(object,shard_rect)
+		#pygame.draw.rect(screen,(126,166,114),shard_rect)
+
+	def randomize(self):
+		self.x = random.randint(0,cell_number - 1)
+		self.y = random.randint(0,cell_number - 1)
+		self.pos = Vector2(self.x,self.y)
+class MAIN:
+	def __init__(self):
+		self.snake = SNAKE()
+		self.shard = SHARD()
+	def update(self):
+		self.snake.move_snake()
+		self.check_collision()
+		self.check_fail()
+
+	def draw_elements(self):
+		self.shard.draw_shard()
+		self.snake.draw_snake()
+		self.draw_score()
+
+	def check_collision(self):
+		if self.shard.pos == self.snake.body[0]:
+			self.shard.randomize()
+			self.snake.add_block()
+			self.snake.play_crunch_sound()
+
+		for block in self.snake.body[1:]:
+			if block == self.shard.pos:
+				self.shard.randomize()
+	def check_fail(self):
+		if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+			self.game_over()
+
+		for block in self.snake.body[1:]:
+			if block == self.snake.body[0]:
+				self.game_over()
+		
+	def game_over(self):
+		self.snake.reset()				
+
+	def draw_score(self):
+		score_text = str(len(self.snake.body) - 3)
+		score_surface = game_font.render(score_text,True,(56,74,12))
+		score_x = int(cell_size * cell_number - 60)
+		score_y = int(cell_size * cell_number - 40)
+		score_rect = score_surface.get_rect(center = (score_x,score_y))
+		object_rect = object.get_rect(midright = (score_rect.left,score_rect.centery))
+		bg_rect = pygame.Rect(object_rect.left,object_rect.top,object_rect.width + score_rect.width + 6,object_rect.height)
+
+		pygame.draw.rect(screen,(167,209,61),bg_rect)
+		screen.blit(score_surface,score_rect)
+		screen.blit(object,object_rect)
+		pygame.draw.rect(screen,(56,74,12),bg_rect,2)
+
+pygame.mixer.pre_init(44100,-16,2,512)
 pygame.init()
-# Kích thước màn hình
-window_width = 600
-window_height = 400
-# Màu
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (213, 50, 80)
-green = (0, 255, 0)
-blue = (50, 153, 213)
-# Tốc độ rắn
-snake_block = 10
-snake_speed = 15
-# Tạo màn hình
-window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption('Snake Game')
-# Đồng hồ
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
+imp = pygame.image.load('D:/study/KHMT/finalPro/space.png').convert()
 clock = pygame.time.Clock()
-# Font chữ
-font_style = pygame.font.SysFont("bahnschrift", 25)
-score_font = pygame.font.SysFont("comicsansms", 35)
-# Điểm số
-def your_score(score):
-    value = score_font.render("Score: " + str(score), True, white)
-    window.blit(value, [0, 0])
-# Vẽ rắn
-def draw_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(window, green, [x[0], x[1], snake_block, snake_block])
-# Hiển thị thông báo
-def message(msg, color):
-    mesg = font_style.render(msg, True, color)
-    window.blit(mesg, [window_width / 6, window_height / 3])
-# Kết thúc game
-def game_loop():
-    game_over = False
-    game_close = False
-    # Tọa độ bắt đầu của rắn
-    x1 = window_width / 2
-    y1 = window_height / 2
-    # Tọa độ di chuyển của rắn
-    x1_change = 0
-    y1_change = 0
-    # Danh sách lưu thân rắn
-    snake_list = []
-    length_of_snake = 1
-    # Vị trí quả táo
-    foodx = round(random.randrange(0, window_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, window_height - snake_block) / 10.0) * 10.0
-    while not game_over:
-        while game_close:
-            window.fill(blue)
-            message("You Lost! Press Q-Quit or C-Play Again", red)
-            your_score(length_of_snake - 1)
-            pygame.display.update()
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        game_loop()
-        # Điều khiển rắn
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
-        # Kiểm tra rắn đi ra khỏi màn hình
-        if x1 >= window_width or x1 < 0 or y1 >= window_height or y1 < 0:
-            game_close = True
-        x1 += x1_change
-        y1 += y1_change
-        window.fill(black)
-        # Vẽ quả táo
-        pygame.draw.rect(window, red, [foodx, foody, snake_block, snake_block])
-        # Thêm thân rắn
-        snake_head = []
-        snake_head.append(x1)
-        snake_head.append(y1)
-        snake_list.append(snake_head)
-        if len(snake_list) > length_of_snake:
-            del snake_list[0]
-        # Kiểm tra rắn cắn thân
-        for x in snake_list[:-1]:
-            if x == snake_head:
-                game_close = True
-        # Vẽ rắn
-        draw_snake(snake_block, snake_list)
-        your_score(length_of_snake - 1)
-        pygame.display.update()
-        # Kiểm tra rắn ăn táo
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, window_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, window_height - snake_block) / 10.0) * 10.0
-            length_of_snake += 1
-        clock.tick(snake_speed)
-    pygame.quit()
-    quit()
-# Chạy game
-game_loop()
+object = pygame.image.load('D:/study/KHMT/finalPro/shard.png').convert_alpha()
+game_font = pygame.font.Font('D:/study/KHMT/finalPro/PoetsenOne-Regular.ttf', 25)
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE,150)
+
+main_game = MAIN()
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+		if event.type == SCREEN_UPDATE:
+			main_game.update()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_UP:
+				if main_game.snake.direction.y != 1:
+					main_game.snake.direction = Vector2(0,-1)
+			if event.key == pygame.K_RIGHT:
+				if main_game.snake.direction.x != -1:
+					main_game.snake.direction = Vector2(1,0)
+			if event.key == pygame.K_DOWN:
+				if main_game.snake.direction.y != -1:
+					main_game.snake.direction = Vector2(0,1)
+			if event.key == pygame.K_LEFT:
+				if main_game.snake.direction.x != 1:
+					main_game.snake.direction = Vector2(-1,0)
+
+	screen.fill((175,215,70))
+	main_game.draw_elements()
+	pygame.display.update()
+	clock.tick(60)
